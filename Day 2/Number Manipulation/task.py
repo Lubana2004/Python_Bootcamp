802f55b62d4f529ace4bfb6705fe6fd5de7fb4c3
Lubana2004 <khan.lubana04@gmail.com>
2026-06-15 02:22:09 +1200

    DAY 2 COMPLETE

--- Day 2/Number Manipulation/task.py
+++ Day 2/Number Manipulation/task.py
@@ -1,1 +1,13 @@
-bmi = 84 / 1.65 ** 2
+# bmi = 84 / 1.65 ** 2
+# print(bmi)
+#
+# print(int(bmi))
+# round(bmi)
+# print(round(bmi))
+#
+# print(round(bmi , 2))
+
+score = 0
+score += 2
+print(score)
+print(6 + 4 / 2 - (1 * 2))
--- Day 2/Type Error, Checking and Conversion/task.py
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -1,1 +1,11 @@
-len(12345)
+# len("Hello")
+# print(type("Hello"))
+# print(type(123))
+# print(type(123.456))
+# print(type(True))
+
+print(int("123") + int("456"))
+
+name_of_user =input("Enter your name")
+length_of_name = len(name_of_user)
+print("Number of letters in your name: " + str(length_of_name))
